84cf4682c4dd1336babeea585acb7cb28e70c3a0
digif <digifi@laposte.net>
2019-03-06 12:16:19 +0100

    cleanup

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject obj2spawn;
+    public float spawnRate;
+    float tmp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tmp= spawnRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tmp -= Time.deltaTime;
+        if (tmp < 0f)
+        {
+            GameObject zombie = Instantiate(obj2spawn, transform.position, transform.rotation)as GameObject;
+            zombie.name = "Zombie";
+            tmp = spawnRate;
+        }
+    }
+}
